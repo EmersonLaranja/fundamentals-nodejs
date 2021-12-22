a73f344eb9103dfc990df31ee6f1a941abe6a0d9
EmersonLaranja <emersonlaranja@gmail.com>
2021-12-22 15:23:05 -0300

    creating an account

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
-
+const { v4: uuidv4 } = require('uuid');
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => { res.json({ message: "Hello World!" }) });
+const customers = [];
+
+
+app.post('/account', (req, res) => {
+  const { cpf, name } = req.body;
+  const id = uuidv4();
+  customers.push({
+    cpf, name, id, statement: []
+  })
+
+  return res.status(201).send();
+})
 
 app.listen(3333);
